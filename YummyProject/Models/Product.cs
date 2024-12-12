a674b00856b1ab3dbbce444a299f0cbe07530cad
mfkahraman <mfurkankahraman@gmail.com>
2024-12-12 22:30:56 +0300

    Add project files.

---
+++ YummyProject/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YummyProject.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ImageUrl { get; set; }
+        public string ProductName { get; set; }
+        public string Ingrdients { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; } //navigation property
+    }
+}
