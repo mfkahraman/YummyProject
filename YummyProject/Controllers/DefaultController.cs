78bba1a34825180e8bf330643f4ec5c310c64197
mfkahraman <mfurkankahraman@gmail.com>
2024-12-17 21:02:03 +0300

    Layout created, DefaultController added, started to create partialviews

---
+++ YummyProject/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YummyProject.Context;
+
+namespace YummyProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly YummyContext db = new YummyContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult DefaultFeature()
+        {
+            var values = db.Features.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult DefaultAbout()
+        {
+            var values = db.Abouts.ToList();
+            return PartialView(values);
+        }
+    }
+}
